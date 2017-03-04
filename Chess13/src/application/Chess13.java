3d85a7de7df0d4512f5f7e1fe5ae389e172f6769
kliu <kliu@KL1600X900>
2017-03-03 19:33:45 -0500

    solve conflicts

---
+++ Chess13/src/application/Chess13.java
@@ -0,0 +1,11 @@
+//By Tim Remmert and Mengyu Gong
+package application;
+
+public class Chess13 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ //this is a test
+	}
+
+}
